7ed580a43efcb338e6297482c868ed6756dc0248
Bruno Lima Cardoso <blimacardoso@gmail.com>
2022-04-03 18:57:57 -0300

    it creates the global agent as a flower server

--- scripts/fl_agents/fl_global_agent.py
+++ scripts/fl_agents/fl_global_agent.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .fl_agent import FLAgent
+import flwr as fl
 
 
-class FLGlobalAgent(FLAgent):
-    """Class used to represent the architecture global (fog) agent"""
+def main() -> None:
+    # Define strategy
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=0.5,
+        fraction_eval=0.5,
+    )
+
+    # Start Flower server for three rounds of federated learning
+    fl.server.start_server(
+        server_address="[::]:8080",
+        config={"num_rounds": 10},
+        strategy=strategy,
+    )
+
+
+if __name__ == "__main__":
+    main()
