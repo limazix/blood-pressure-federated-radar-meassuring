a0337585c658351a475aa008b0435c26c333276e
Bruno Lima Cardoso <blimacardoso@gmail.com>
2022-04-05 16:57:51 -0300

    it creates the subject data dir path on single runner

--- scripts/run_single.py
+++ scripts/run_single.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import click
+
+from utils.validator import validate_directory_path
 
 
 @click.command()
-@click.option('--data-dir')
-@click.option('--subject-id')
-@click.option('--port')
+@click.option("--data-dir")
+@click.option("--subject-id")
+@click.option("--port")
 def main(data_dir, subject_id, port):
-    print(data_dir, subject_id, port)
+    subject_data_dir = os.path.normpath(os.path.join(data_dir, subject_id))
+    validate_directory_path(subject_data_dir)
 
 
 if __name__ == "__main__":
