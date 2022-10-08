d05784e20be7b09f11738a66817a66e7c9517479
Bruno Lima Cardoso <blimacardoso@gmail.com>
2022-10-08 16:35:38 -0300

    it creates a class to build the machine learning data loaders based on a given radar and blood preassure data

---
+++ scripts/data_transforms/data_loader_builder.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+
+import numpy as np
+
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+
+from data_models.subject_dataset import SubjectDataset
+
+from utils.configurator import config
+
+from .to_tensor import ToTensor
+from .normalize import Normalize
+from .fill_nan import FillNan
+
+
+class DataLoaderBuilder:
+    def build_dataloader(self, dataset, start_index, end_index):
+        _dataset = copy.deepcopy(dataset)
+        _dataset.prune(start_index, end_index)
+        return DataLoader(
+            dataset=_dataset,
+            shuffle=False,
+            num_workers=int(config["dataloader"]["num_workers"]),
+            batch_size=int(config["dataloader"]["batch_size"]),
+        )
+
+    def build_loaders(self, radar, bp):
+        radar_mean, radar_std = np.nanmean(radar, axis=0), np.nanstd(radar, axis=0)
+        bp_mean, bp_std = np.nanmean(bp, axis=0), np.nanstd(bp, axis=0)
+
+        dataset = SubjectDataset(
+            radar=radar,
+            radar_sr=int(config["dataset"]["radar_sr"]),
+            bp=bp,
+            bp_sr=int(config["dataset"]["bp_sr"]),
+            window_size=float(config["dataset"]["window_size"]),
+            overlap=float(config["dataset"]["overlap"]),
+            transform=Compose(
+                [
+                    ToTensor(),
+                    FillNan(default_mean=radar_mean),
+                    Normalize(mean=radar_mean, std=radar_std),
+                ]
+            ),
+            target_transform=Compose(
+                [
+                    ToTensor(),
+                    FillNan(default_mean=bp_mean),
+                    Normalize(mean=bp_mean, std=bp_std),
+                ]
+            ),
+        )
+        data_size = len(dataset)
+
+        end_train_index = int(data_size * float(config["setup"]["train_size"]))
+        end_val_index = end_train_index + int(
+            data_size * float(config["setup"]["val_size"])
+        )
+
+        train_loader = self.build_dataloader(
+            dataset, start_index=0, end_index=end_train_index
+        )
+        val_loader = self.build_dataloader(
+            dataset, start_index=end_train_index, end_index=end_val_index
+        )
+        test_loader = self.build_dataloader(
+            dataset, start_index=end_val_index, end_index=data_size
+        )
+        return dataset, train_loader, val_loader, test_loader
--- scripts/main.py
+++ scripts/main.py
@@ -2,24 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
-import copy
 import click
-
-import numpy as np
-
-from torch.utils.data import DataLoader
-from torchvision.transforms import Compose
 
 import flwr as fl
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 from data_models.data_builder import DataBuilder
-from data_models.subject_dataset import SubjectDataset
 
-from data_transforms.to_tensor import ToTensor
-from data_transforms.normalize import Normalize
-from data_transforms.fill_nan import FillNan
+from data_transforms.data_loader_builder import DataLoaderBuilder
 
 from ml_models.model_builder import ModelBuilder
 
@@ -29,64 +20,15 @@ from fl_agents.fl_global_agent import run_global_agent
 from utils.configurator import config
 
 
-def build_dataloader(dataset, start_index, end_index):
-    _dataset = copy.deepcopy(dataset)
-    _dataset.prune(start_index, end_index)
-    return DataLoader(
-        dataset=_dataset,
-        shuffle=False,
-        num_workers=int(config["dataloader"]["num_workers"]),
-        batch_size=int(config["dataloader"]["batch_size"]),
-    )
-
-
-def build_loaders(radar, bp):
-    radar_mean, radar_std = np.nanmean(radar, axis=0), np.nanstd(radar, axis=0)
-    bp_mean, bp_std = np.nanmean(bp, axis=0), np.nanstd(bp, axis=0)
-
-    dataset = SubjectDataset(
-        radar=radar,
-        radar_sr=int(config["dataset"]["radar_sr"]),
-        bp=bp,
-        bp_sr=int(config["dataset"]["bp_sr"]),
-        window_size=float(config["dataset"]["window_size"]),
-        overlap=float(config["dataset"]["overlap"]),
-        transform=Compose(
-            [
-                ToTensor(),
-                FillNan(default_mean=radar_mean),
-                Normalize(mean=radar_mean, std=radar_std),
-            ]
-        ),
-        target_transform=Compose(
-            [
-                ToTensor(),
-                FillNan(default_mean=bp_mean),
-                Normalize(mean=bp_mean, std=bp_std),
-            ]
-        ),
-    )
-    data_size = len(dataset)
-
-    end_train_index = int(data_size * float(config["setup"]["train_size"]))
-    end_val_index = end_train_index + int(
-        data_size * float(config["setup"]["val_size"])
-    )
-
-    train_loader = build_dataloader(dataset, start_index=0, end_index=end_train_index)
-    val_loader = build_dataloader(
-        dataset, start_index=end_train_index, end_index=end_val_index
-    )
-    test_loader = build_dataloader(
-        dataset, start_index=end_val_index, end_index=data_size
-    )
-    return dataset, train_loader, val_loader, test_loader
-
-
 def run_lightning():
     data_builder = DataBuilder()
     radar, bp = data_builder.get_data()
-    dataset, train_loader, val_loader, test_loader = build_loaders(radar, bp)
+
+    data_loader_builder = DataLoaderBuilder()
+    dataset, train_loader, val_loader, test_loader = data_loader_builder.build_loaders(
+        radar, bp
+    )
+
     model_builder = ModelBuilder()
     model = model_builder.build(dataset)
 
@@ -103,7 +45,15 @@ def run_lightning():
 def run_local_agent(subject_id):
     data_builder = DataBuilder()
     radar, bp = data_builder.get_data(subject_id)
-    subject_dataset, train_loader, val_loader, test_loader = build_loaders(radar, bp)
+
+    data_loader_builder = DataLoaderBuilder()
+    (
+        subject_dataset,
+        train_loader,
+        val_loader,
+        test_loader,
+    ) = data_loader_builder.build_loaders(radar, bp)
+
     model_builder = ModelBuilder()
     model = model_builder.build(subject_dataset)
 
